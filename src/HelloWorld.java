67550421eed88093d670dad8f047570e9b795ab1
MarkHiarley <hiarleycostamarley@gmail.com>
2025-12-28 12:45:39 -0300

    First commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+   public String latir(){
+       return "AU";
+   }
+
+
+
+  public static void main(String[] args){
+        HelloWorld ca = new HelloWorld();
+      String somDoCachorro = ca.latir();
+
+      System.out.println("o som retonrando do achorro foi:"+ somDoCachorro);
+
+
+  }
+
+
+}
+
